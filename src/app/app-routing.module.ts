dafca3a95f19b3ade51541caba07479ce311890e
Ingrid J <ingrijrm94@gmail.com>
2021-11-02 22:15:44 -0500

    se agrego elementos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CardsComponent } from './components/cards/cards.component';
 
-const routes: Routes = [
-  {path: '', component: CardsComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
-import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    CardsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
