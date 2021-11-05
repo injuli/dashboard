9d8376272349f1637ca5d40d8be684e506d5e3f5
ingrid <iruiz.playtechla@gmail.com>
2021-11-05 08:36:37 -0500

    cambios prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {APP_BASE_HREF} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +20,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
